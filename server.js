fe7d758e23ae3bc9b09d50041fe5168786abf998
Joseph Knowles <joejknowles@gmail.com>
2017-02-18 00:34:53 +0000

    hello world server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('/', ((req, res)=> res.json({ hi: 'hi' })))
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+});
