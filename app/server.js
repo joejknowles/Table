520d47c393ba9f034dab05fa37cc30914ab180ec
Joseph Knowles <joejknowles@gmail.com>
2017-02-21 23:42:16 +0000

    serve client from express server

--- app/server.js
+++ app/server.js
@@ -4,11 +4,7 @@ const app = express();
 
 app.set('port', (process.env.PORT || 3001));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
-}
-
-app.get('/', ((req, res)=> res.json({ hi: 'hi' })))
+app.use(express.static('client/build'));
 
 const server = app.listen(app.get('port'), () => {
   console.log(`Express listening at: http://localhost:${app.get('port')}/`);
