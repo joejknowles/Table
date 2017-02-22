cdb68446fbc9011523bd7e16efce205b4c923472
Joseph Knowles <joejknowles@gmail.com>
2017-02-22 10:39:45 +0000

    add app snapshot test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ReactTestUtils from 'react-addons-test-utils';
+const shallowRenderer = ReactTestUtils.createRenderer();
+
+it('App renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('App renders as before', () => {
+  const app = shallowRenderer.render(<App />);
+  expect(app).toMatchSnapshot();
 });
