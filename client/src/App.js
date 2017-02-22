adfb86d8f92ddea8faf92b5d97d1d55cc69e9fbc
Joseph Knowles <joejknowles@gmail.com>
2017-02-22 14:40:13 +0000

    add react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import * as res from '../../resources/pages/startScreen';
+
+import { Router, Route, browserHistory } from 'react-router';
+import StartScreen from './components/startScreen';
+import Play from './components/play';
+
+import paths from './paths'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <button className="player-join-button">{ res.playerJoinButton }</button>
-      </div>
+      <Router history={ browserHistory }>
+        <Route path={ paths.startScreen } component={ StartScreen } />
+        <Route path={ paths.play } component={ Play } />
+      </Router>
     );
   }
 }
---
+++ client/src/paths.js
@@ -0,0 +1,4 @@
+export default {
+  startScreen: '/',
+  play: '/play'
+}
