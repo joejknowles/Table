45a3e16a0ab4fb07e8cc5c104a90d64e52bc0a5b
Joseph Knowles <joejknowles@gmail.com>
2017-02-23 17:46:44 +0000

    send index.html file to all urls

--- app/server.js
+++ app/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.set('port', (process.env.PORT || 3001));
 
 app.use(express.static('client/build'));
 
-const server = app.listen(app.get('port'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
+const logServerListening = () => {
+  console.log(`Express listening at: http://localhost:${app.get('port')}/`);
+};
+
+const server = app.listen(app.get('port'), logServerListening);
+
+server.on('error', (e) => {
+  if (e.code == 'EADDRINUSE') {
+    console.log('Address in use, retrying...');
+      server.close();
+    setTimeout(() => {
+      server.listen(PORT, HOST);
+      logServerListening();
+    }, 100);
+  }
+});
 
 module.exports = {
   server: server
