7acdf52ebef4e02e0590f11bed45448fe4239b17
Joseph Knowles <joejknowles@gmail.com>
2017-02-24 14:48:40 +0000

    remove listening from app creation

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
+module.exports = app;
