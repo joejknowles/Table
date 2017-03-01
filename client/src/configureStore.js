fc2e0af967dd78356312c54389bb4383aa460de3
Joseph Knowles <joejknowles@gmail.com>
2017-03-01 21:36:11 +0000

    add redux-saga

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,9 +1,10 @@
 import reducer from './reducers';
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import createLogger from 'redux-logger';
 
 export default () => {
-  const middleware = [];
+  const middleware = [ createSagaMiddleware() ];
   if (process.env.NODE_ENV !== 'production') {
     middleware.push(createLogger());
   }
