cc593bf021c6c0cfb62dc405b9d65d6c0068d427
Joseph Knowles <joejknowles@gmail.com>
2017-03-05 12:46:23 +0000

    reword tests

--- tests/_features_/joinGame.test.js
+++ tests/_features_/joinGame.test.js
@@ -12,17 +12,16 @@ beforeAll(async () => appStarter = await createAppStarter(5050));
 beforeEach(async () =>{
   host = appStarter();
   browser = await createBrowser(host.port);
+    await browser.visit(paths.startScreen);
 });
 
-it('clicking "join as player" button shows a deck of cards', async () => {
-  await browser.visit('/');
+it('clicking "join as player" button shows a play card button', async () => {
   const playerJoinButton = await browser.click('.player-join-button');
-  const cardDeck = await browser.find('.play-card');
-  expect(cardDeck.className).toBe('btn play-card');
+  const button = await browser.find('.play-card');
+  expect(button.className).toBe('btn play-card');
 });
 
-it('clicking "join as player" button shows a deck of cards', async () => {
-  await browser.visit('/');
+it('clicking "join as table" button shows a deck of cards', async () => {
   const playerJoinButton = await browser.click('.table-join-button');
   const cardDeck = await browser.find('.pile');
   expect(cardDeck.className).toBe('pile');
