21dd06a1d05f8c0939c9ebb8b3b64883059fa115
Joseph Knowles <joejknowles@gmail.com>
2017-03-08 23:01:48 +0000

    display game code

--- client/src/components/waiting.js
+++ client/src/components/waiting.js
@@ -7,8 +7,9 @@ import { gameCodeSelector } from '../reducers';
 
 export const Waiting = ({ code, players }) => (
   <div className="App">
-    <p className="game-code" >{ code }</p>
     <p className="number-of-players">{ res.numOfPlayers(players) }</p>
+    <p className="game-code-message" >{ res.enterGameCodeMessage }</p>
+    <p className="game-code" >{ res.gameCode(code) }</p>
     <DispatchButton
       res={ res.begin }
       action={ { type: 'REQUEST_BEGIN_GAME' } }
--- client/src/resources/pages/waiting.js
+++ client/src/resources/pages/waiting.js
@@ -1,4 +1,6 @@
 export default {
   begin: 'Begin Game',
-  numOfPlayers: (num) => `${ num } players joined so far`
+  numOfPlayers: (num) => `${ num } players joined so far`,
+  enterGameCodeMessage: 'Enter this game code on another device to play',
+  gameCode: (code) => `code: ${ code }`
 }
