d1f69acbb850bc441e8a691c640b26683dde3cdc
Joseph Knowles <joejknowles@gmail.com>
2017-03-11 18:19:50 +0000

    add delay to async rechecks

--- tests/_features_/setup/asyncHelpers.js
+++ tests/_features_/setup/asyncHelpers.js
@@ -1,9 +1,14 @@
+const sleep = (ms = 0) => {
+  return new Promise(r => setTimeout(r, ms));
+}
+
 export const recheck = async(boolTest) => {
   let result = await boolTest();
   for (let attempts = 0; attempts < 5; attempts++) {
     if (result) {
       return result
     } else {
+      await sleep(500);
       result = await boolTest();
     }
   }
