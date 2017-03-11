56910f248971b6ee674c909400fddb49b4ef45b5
Joseph Knowles <joejknowles@gmail.com>
2017-03-11 16:30:49 +0000

    add more cards

--- app/game/cards.js
+++ app/game/cards.js
@@ -48,5 +48,105 @@ module.exports = [
     id: 10,
     suit: 'SPADES',
     rank: '5'
+  },
+  {
+    id: 1,
+    suit: 'HEARTS',
+    rank: 'A'
+  },
+  {
+    id: 2,
+    suit: 'HEARTS',
+    rank: '2'
+  },
+  {
+    id: 3,
+    suit: 'HEARTS',
+    rank: '3'
+  },
+  {
+    id: 4,
+    suit: 'HEARTS',
+    rank: '4'
+  },
+  {
+    id: 5,
+    suit: 'HEARTS',
+    rank: '5'
+  },
+  {
+    id: 6,
+    suit: 'SPADES',
+    rank: 'A'
+  },
+  {
+    id: 7,
+    suit: 'SPADES',
+    rank: '2'
+  },
+  {
+    id: 8,
+    suit: 'SPADES',
+    rank: '3'
+  },
+  {
+    id: 9,
+    suit: 'SPADES',
+    rank: '4'
+  },
+  {
+    id: 10,
+    suit: 'SPADES',
+    rank: '5'
+  },
+  {
+    id: 1,
+    suit: 'HEARTS',
+    rank: 'A'
+  },
+  {
+    id: 2,
+    suit: 'HEARTS',
+    rank: '2'
+  },
+  {
+    id: 3,
+    suit: 'HEARTS',
+    rank: '3'
+  },
+  {
+    id: 4,
+    suit: 'HEARTS',
+    rank: '4'
+  },
+  {
+    id: 5,
+    suit: 'HEARTS',
+    rank: '5'
+  },
+  {
+    id: 6,
+    suit: 'SPADES',
+    rank: 'A'
+  },
+  {
+    id: 7,
+    suit: 'SPADES',
+    rank: '2'
+  },
+  {
+    id: 8,
+    suit: 'SPADES',
+    rank: '3'
+  },
+  {
+    id: 9,
+    suit: 'SPADES',
+    rank: '4'
+  },
+  {
+    id: 10,
+    suit: 'SPADES',
+    rank: '5'
   }
 ];
