3f551eb7bb2730eb7357e13cf3c96063c187db39
Joseph Knowles <joejknowles@gmail.com>
2017-03-11 14:03:13 +0000

    begin game only starts specific game

--- client/src/sagas/beginGame.js
+++ client/src/sagas/beginGame.js
@@ -1,12 +1,14 @@
 import { createChannelForEvent } from './socketEvents';
-import { put, call, takeEvery } from 'redux-saga/effects';
+import { put, call, takeEvery, select } from 'redux-saga/effects';
+import { gameCodeSelector } from '../reducers';
 
 export const createBeginGameChannel = (socket) => {
   return createChannelForEvent(socket, 'BEGIN_GAME')
 }
 
 export function* requestBeginGame(socket) {
-  yield call([socket, socket.emit], 'REQUEST_BEGIN_GAME');
+  const gameCode = yield select(gameCodeSelector)
+  yield call([socket, socket.emit], 'REQUEST_BEGIN_GAME', { gameCode });
 }
 
 export function* beginGame() {
