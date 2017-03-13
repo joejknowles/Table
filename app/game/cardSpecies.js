d547a5c2cf4807b8ad5f5cf746262b1f2e4d481e
Joseph Knowles <joejknowles@gmail.com>
2017-03-13 15:29:03 +0000

    fix cards

--- app/game/cardSpecies.js
+++ app/game/cardSpecies.js
@@ -3,23 +3,24 @@ const pictureRanks = {
   1: 'A',
   11: 'J',
   12: 'Q',
-  13: 'k'
+  13: 'K'
 };
+const isPictureCard = (rankNo) => pictureRanks.hasOwnProperty(rankNo);
+
 const deck = [];
 for (let suitNo = 0; suitNo < 4; suitNo++) {
   const suit = suits[suitNo];
-
   for (let rankNo = 1; rankNo < 14; rankNo++) {
     let rank;
-    if (pictureRanks.hasOwnProperty(rankNo)) {
+    if (isPictureCard(pictureRanks)) {
       rank = pictureRanks[rankNo];
     } else {
       rank = rankNo.toString();
     }
     const card = {
       id: rankNo + (suitNo * 13),
-      suit: suit,
-      ranks: rank
+      suit,
+      rank
     };
     deck.push(card);
   }
