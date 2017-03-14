06f985c19565c28d0a80da257f708360d8194f26
Joseph Knowles <joejknowles@gmail.com>
2017-03-14 10:41:33 +0000

    add snap snapshot

--- client/src/tests/test_components/play/index.test.js
+++ client/src/tests/test_components/play/index.test.js
@@ -1,5 +1,6 @@
 import { Play } from '../../../components/play';
 import PlayCardButton from '../../../components/play/playCardButton';
+import Snap from '../../../components/play/snap';
 import React from 'react';
 
 import ReactTestUtils from 'react-addons-test-utils';
@@ -20,5 +21,11 @@ describe('Play page', () => {
 
 it('PlayCardButton renders as before', () => {
   const component = shallowRenderer.render(<PlayCardButton />);
+  expect(component).toMatchSnapshot();
+});
+
+
+it('Snap renders as before', () => {
+  const component = shallowRenderer.render(<Snap />);
   expect(component).toMatchSnapshot();
 });
