c85a5832205ec3c8c41bd7b78abb145d46ffad42
Joseph Knowles <joejknowles@gmail.com>
2017-03-19 21:30:14 +0000

    add card swipe library

---
+++ client/src/lib/swipe-cards/index.js
@@ -0,0 +1,9 @@
+import Cards from './Cards'
+
+import CardImport from './CardSwitcher'
+
+export const Card = CardImport;
+
+export default Cards
+
+// stolen from- https://github.com/MrToph/react-swipe-card
